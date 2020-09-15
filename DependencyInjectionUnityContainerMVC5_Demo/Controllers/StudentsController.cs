faf42f05b774fbf2cc3d11bd3a0ae9a02145ad95
Farhan Ahmed <farhan1086@gmail.com>
2020-09-15 20:59:59 +0530

    Add project files.

---
+++ DependencyInjectionUnityContainerMVC5_Demo/Controllers/StudentsController.cs
@@ -0,0 +1,112 @@
+﻿using CURDWithEntityFrameworkCodeFirstWebAPI_Demo.Models;
+using DependencyInjectionUnityContainerMVC5_Demo.Repositories;
+using System.Net;
+using System.Web.Mvc;
+
+namespace DependencyInjectionUnityContainerMVC5_Demo.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private UnitOfWork unitOfWork;
+        public StudentsController(UnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+        public ActionResult Index()
+        {
+            var students =unitOfWork.StudentRepository.GetAll();
+            return View(students);
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student =unitOfWork.StudentRepository.Get(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.CourseId = new SelectList(unitOfWork.CourseRepositroy.GetAll(), "Id", "CourseName");
+            ViewBag.InstructorId = new SelectList(unitOfWork.InstructorRepository.GetAll(), "Id", "InstructorName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.StudentRepository.Add(student);
+                unitOfWork.Complete();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CourseId = new SelectList(unitOfWork.CourseRepositroy.GetAll(), "Id", "CourseName");
+            ViewBag.InstructorId = new SelectList(unitOfWork.InstructorRepository.GetAll(), "Id", "InstructorName");
+            return View(student);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student =unitOfWork.StudentRepository.Get(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CourseId = new SelectList(unitOfWork.CourseRepositroy.GetAll(), "Id", "CourseName");
+            ViewBag.InstructorId = new SelectList(unitOfWork.InstructorRepository.GetAll(), "Id", "InstructorName");
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                unitOfWork.StudentRepository.Update(student);
+                return RedirectToAction("Index");
+            }
+            ViewBag.CourseId = new SelectList(unitOfWork.CourseRepositroy.GetAll(), "Id", "CourseName");
+            ViewBag.InstructorId = new SelectList(unitOfWork.InstructorRepository.GetAll(), "Id", "InstructorName");
+            return View(student);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Student student = unitOfWork.StudentRepository.Get(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var entity = unitOfWork.StudentRepository.Get(id);
+            unitOfWork.StudentRepository.Remove(entity);
+            unitOfWork.Complete();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DependencyInjectionUnityContainerMVC5_Demo/Repositories/Repository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DependencyInjectionUnityContainerMVC5_Demo.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        public Repository(DbContext context)
+        {
+            Context = context;
+        }
+        public TEntity Get(int? id)
+        {
+            return Context.Set<TEntity>().Find(id);
+        }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Context.Set<TEntity>().ToList();
+        }
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().Where(predicate);
+        }
+        public void Add(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+        }
+        public void Update(TEntity entity)
+        {
+            Context.Set<TEntity>().AddOrUpdate(entity);
+        }
+        public void Remove(TEntity entity)
+        {
+            Context.Set<TEntity>().Remove(entity);
+        }
+        public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Context.Set<TEntity>().SingleOrDefault(predicate);
+        }
+    }
+}
